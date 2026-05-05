a9dc952493e84776fcdaf45efd3fe9b540cb55b1
kihoto <george.njenga@student.moringaschool.com>
2026-05-05 21:23:16 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -27,3 +27,21 @@ async function saveUserToDB(user, extraData = {}) {
         throw error;
     }
 }
+
+if (isLogin) {
+    userCredential = await signInWithEmailAndPassword(auth, email, password);
+} else {
+    userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+    const user = userCredential.user;
+
+    await set(ref(db, 'users/' + user.uid), {
+        username: email.split('@')[0],
+        email: email,
+        lastLogin: Date.now()
+    });
+}
+
+//  Redirect here
+window.location.href = "pollform.html";
+export default index.js;

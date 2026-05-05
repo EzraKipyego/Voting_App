151f03c9843f72319c194942f4be09f6b237a834
EZRA KIPYEGO <ezra.kipyego@student.moringaschool.com>
2026-05-05 10:18:51 +0300

    set up the deployment for github action

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,24 @@
 // })
 
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//     base: "/Voting_App/",
+//   server: {
+//     port: 3000
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    base: "/Voting_App/",
-  server: {
-    port: 3000
-  }
+  base: "/Voting_App/"
 })

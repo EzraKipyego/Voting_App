e97a31e6c0e0e04a3423fefd81010cdb3173989e
EZRA KIPYEGO <ezra.kipyego@student.moringaschool.com>
2026-05-05 09:12:56 +0300

    Modified to prevent existing poll option

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
     return saved
       ? JSON.parse(saved)
       : [
-          { id: 1, text: "Class Representative A", votes: 0 },
-          { id: 2, text: "Class Representative B", votes: 0 },
-          { id: 3, text: "Class Representative C", votes: 0 },
+          { id: 1, text: "Class Representative A (Kelvin Omondi)", votes: 0 },
+          { id: 2, text: "Class Representative B (Precious Njeru)", votes: 0 },
+          { id: 3, text: "Class Representative C(George Mwangi)", votes: 0 },
         ];
   });
 
@@ -18,17 +18,33 @@ function App() {
     return JSON.parse(localStorage.getItem("hasVoted")) || false;
   });
 
+  // Tracks all the  votes 
+  const [voteHistory, setVoteHistory] = useState(() => {
+    return JSON.parse(localStorage.getItem("voteHistory")) || 0;
+  });
+
   useEffect(() => {
     localStorage.setItem("pollOptions", JSON.stringify(options));
     localStorage.setItem("hasVoted", JSON.stringify(hasVoted));
-  }, [options, hasVoted]);
+    localStorage.setItem("voteHistory", JSON.stringify(voteHistory));
+  }, [options, hasVoted, voteHistory]);
 
   const addOption = (text) => {
     if (!text.trim()) return;
 
+    // Prevent duplicate poll options (case-insensitive)
+    const exists = options.some(
+      (opt) => opt.text.toLowerCase() === text.trim().toLowerCase()
+    );
+
+    if (exists) {
+      alert("This poll option already exists!");
+      return;
+    }
+
     const newOption = {
       id: Date.now(),
-      text,
+      text: text.trim(),
       votes: 0,
     };
 
@@ -44,6 +60,9 @@ function App() {
 
     setOptions(updated);
     setHasVoted(true);
+
+  
+    setVoteHistory(voteHistory + 1);
   };
 
   const resetVotes = () => {
@@ -60,15 +79,18 @@ function App() {
   const totalVotes = options.reduce((sum, opt) => sum + opt.votes, 0);
 
   return (
-  
     <div className="min-h-screen bg-slate-100 flex flex-col justify-center items-center p-4">
-  <h1 className="text-2xl font-bold mb-4">Group 6</h1>
+      <h1 className="text-2xl font-bold mb-4">Group 6</h1>
 
-  <div className="w-full max-w-xl bg-white shadow-lg rounded-xl p-6">
-
-        <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
+      <div className="w-full max-w-xl bg-white shadow-lg rounded-xl p-6">
+        <h1 className="text-3xl font-bold text-center text-blue-600 mb-2">
           Class Representative Voting App
         </h1>
+
+        {/* Historical Vote Tracking */}
+        <p className="text-center text-gray-600 mb-6">
+          Total Votes Ever Cast: {voteHistory}
+        </p>
 
         <PollForm addOption={addOption} />
 
@@ -85,7 +107,6 @@ function App() {
         >
           Reset Votes
         </button>
-
       </div>
     </div>
   );
